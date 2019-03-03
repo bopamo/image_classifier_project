3572891ecc7a6bb1c24a2dc1f81500d59cabe1a2
Bopamo Osaisai <bopamo@gmail.com>
2019-03-03 12:00:27 -0800

    Updating logger

--- logger.py
+++ logger.py
@@ -28,23 +28,23 @@ from coloredlogs import ColoredFormatter
 # logger.addHandler(ch)
 
 
-
-import logging
-LOG_LEVEL = logging.DEBUG
-
-F =  "  %(log_color)s%(levelname)-8s%(reset)s | %(log_color)s%(message)s%(reset)s"
-LOGFORMAT = "[%(levelname)s]  \t%(filename)s : %(funcName)s() : line #: %(lineno)d | %(message)s"
-from colorlog import ColoredFormatter
-logging.root.setLevel(LOG_LEVEL)
-formatter = ColoredFormatter(LOGFORMAT)
-stream = logging.StreamHandler()
-stream.setLevel(LOG_LEVEL)
-stream.setFormatter(formatter)
-logger = logging.getLogger('pythonConfig')
-logger.setLevel(LOG_LEVEL)
-logger.addHandler(stream)
-
-spacing_string = "\n"
+#
+# import logging
+# LOG_LEVEL = logging.DEBUG
+#
+# F =  "  %(log_color)s%(levelname)-8s%(reset)s | %(log_color)s%(message)s%(reset)s"
+# LOG_FORMAT = "[%(levelname)s]  \t%(filename)s : %(funcName)s() : line #: %(lineno)d | %(message)s"
+# from colorlog import ColoredFormatter
+# logging.root.setLevel(LOG_LEVEL)
+# formatter = ColoredFormatter(LOG_FORMAT)
+# stream = logging.StreamHandler()
+# stream.setLevel(LOG_LEVEL)
+# stream.setFormatter(formatter)
+# logger = logging.getLogger('pythonConfig')
+# logger.setLevel(LOG_LEVEL)
+# logger.addHandler(stream)
+#
+# spacing_string = "\n"
 
 
 # import logging
@@ -83,3 +83,43 @@ spacing_string = "\n"
 #########################################################################
 #########################################################################
 #########################################################################
+
+import logging
+
+spacing_string = "\n"
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+# create file handler which logs even debug messages
+# fh = logging.FileHandler('logs.log')
+# fh.setLevel(logging.DEBUG)
+
+# create console handler with a higher log level
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+
+# create a logging format
+F = "  %(log_color)s%(levelname)-8s%(reset)s | %(log_color)s%(message)s%(reset)s"
+LOG_FORMAT = "[%(levelname)s]  \t%(filename)s : %(funcName)s() : line #: %(lineno)d | %(message)s"
+
+
+# create formatter and add it to the handlers
+
+formatter = logging.Formatter(LOG_FORMAT)
+# fh.setFormatter(formatter)
+ch.setFormatter(formatter)
+
+# add the handlers to the logger
+# logger.addHandler(fh)
+logger.addHandler(ch)
+
+
+def log_program_step(debug_string):
+
+    print(spacing_string)
+    logger.info(debug_string)
+    print(spacing_string)
+
+    return False
